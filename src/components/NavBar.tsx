0e5a6b52cb22a8a6c45ac1eb650e34a066ac129e
Thomas Arsanious <tomyarsanious@gmail.com>
2026-06-23 04:26:57 +0300

    Changed The Logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/NewLogo.webp";
+import logo from "../assets/HubLogo.webp";
 
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="80px" />
+      <Image
+        src={logo}
+        boxSize="60px"
+        objectFit="contain"
+        borderRadius="12px"
+      />
       <ColorModeSwitch />
     </HStack>
   );
